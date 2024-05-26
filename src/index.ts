3336b1b6d255c3e88aa0a3c0d91e9f7eca3cef6e
NappySprout <sleepylog20@gmail.com>
2024-05-27 00:17:11 +0800

    configured webhook, bot is live

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Bot, Context, webhookCallback } from "grammy";
+
+export interface Env {
+	BOT_INFO: string;
+	BOT_TOKEN: string;
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(
+		request: Request,
+		env: Env,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		const bot = new Bot(env.BOT_TOKEN, { botInfo: JSON.parse(env.BOT_INFO) });
+
+		bot.command("start", async (ctx: Context) => {
+			await ctx.reply("Hello, world!");
+		});
+
+		return webhookCallback(bot, "cloudflare-mod")(request);
 	},
 };
