759dbaff90ead7d5a00106607533b6a41b2599c7
NappySprout <sleepylog20@gmail.com>
2024-06-01 16:09:01 +0800

    Updated the start command to use the Bot and D1Database classes.

--- src/cmds/start.ts
+++ src/cmds/start.ts
@@ -1,5 +1,12 @@
-import { Context } from "grammy";
-export async function start(ctx: Context) {
-	await ctx.db.put("lift_data", { chat_id: ctx.chat.id, bench_one_rep_max: 0, deadlift_one_rep_max: 0, squat_one_rep_max: 0, shoulder_press_one_rep_max: 0 });
-	await ctx.reply("Success!")
+import { Context, Bot } from "grammy";
+export function start(bot: Bot, db: D1Database) {
+	bot.command("start", async (ctx: Context) => {
+		const chat_id = ctx.chatId
+		const { results } = await db.prepare(
+			//sql query to insert chat_id and set all main lift int to 0 if chat_id already exist in db do nothing
+		)
+			.bind(chat_id)
+			.all();
+		await ctx.reply(results.toString())
+	})
 }
